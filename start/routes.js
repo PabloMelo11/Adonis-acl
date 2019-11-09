d33c82ef5b6836348ab991107bf6ef891ed5ef27
PabloMelo11 <pablomelo418@gmail.com>
2019-11-08 23:36:45 -0300

    Add session JWT

--- start/routes.js
+++ start/routes.js
@@ -1,4 +1,6 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
+Route.post('/sessions', 'SessionController.store')
+
 Route.post('/users', 'UserController.store')
